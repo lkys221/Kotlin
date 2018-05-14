28c3e0ec7b55b1bcb29bfde0f8b69fe75d703f65
Leo .Kim <lkys221@snu.ac.kr>
2018-05-14 22:27:07 +0900

    test

--- test/src/Test.kt
+++ test/src/Test.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-    println("hello world")
+    val valid = listOf(3,6,9)
+    val x = 3
+    when (x) {
+        //in valid -> print("x is valid")
+        in 1..10 -> print("in range")
+        !in 10..20 -> print("not in rage")
+        else -> print("none")
+    }
 }
